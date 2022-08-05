b6e3329f238626c5d7a540c11780790a4a4a1ae2
wawan-ikhwan <ikhwanperwira@gmail.com>
2022-08-06 03:50:03 +0700

    update gitignore

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "uwc_task.h"
 
 void app_main(void) {
-  ESP_LOGI(uwc_tag, "Start of program!");
+  ESP_LOGI(uwc_tag, "Start of app_main()!");
 
   uwc_task_startup();
   vTaskDelay(100 / portTICK_PERIOD_MS);
@@ -11,5 +11,5 @@ void app_main(void) {
 
   uwc_event_udp_init();
 
-  ESP_LOGI(uwc_tag, "End of program!");
+  ESP_LOGI(uwc_tag, "End of app_main()");
 }

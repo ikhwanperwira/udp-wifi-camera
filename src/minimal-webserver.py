d10d87ee5fa9b30a75a3d5bd1df7133232ef44a9
wawan-ikhwan <ikhwanperwira@gmail.com>
2022-08-06 19:33:28 +0700

    support many clients

--- src/minimal-webserver.py
+++ src/minimal-webserver.py
@@ -1,69 +1,85 @@
-def frame_collector(frame): # process 1
-  from io import BytesIO
-  from socket import socket, AF_INET, SOCK_DGRAM
-  SERV_IPV4, SERV_PORT = ('192.168.43.150', 39876)
-  udpSock = socket(AF_INET, SOCK_DGRAM)
+from time import sleep
+from multiprocessing import Pipe
+
+def frame_collector(clients):
+  import io
+  import socket
+  SERV_IPV4, SERV_PORT = ('192.168.145.200', 8888)
+  udpSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   udpSock.bind((SERV_IPV4,SERV_PORT))
   isWriting = False
-  while 1:
-    dataRecv, CLNT_ADDR = udpSock.recvfrom(65507)
-      
-    if not isWriting:
-      if dataRecv[:2] == b'\xff\xd8': # Start of JPEG
-        isWriting = True
-        buf = BytesIO()
+  isConnected = False
+  try:
+    while 1:
+      dataRecv, CLNT_ADDR = udpSock.recvfrom(65507)
 
-    if isWriting:
-      buf.write(dataRecv)
-      if dataRecv[-2:] == b'\xff\xd9': # End of JPEG
-        isWriting = False
-        buf.seek(0)
-        frame.put(buf.read())
-        # open('test/pics/'+str(time())+'.jpg','wb').write(buf.read())
+      if not isConnected:
+        if dataRecv == b'SYN\n':
+          udpSock.sendto(b'ACK\n',CLNT_ADDR)
+          udpSock.sendto(b'SOH\n',CLNT_ADDR)
+          isConnected = True
 
-def flask_service(frame): # process 2
+      if not isWriting:
+        if dataRecv[:2] == b'\xff\xd8': # Start of JPEG
+          isWriting = True
+          buf = io.BytesIO()
+
+      if isWriting:
+        buf.write(dataRecv)
+        if dataRecv[-2:] == b'\xff\xd9': # End of JPEG
+          isWriting = False
+          buf.seek(0)
+          frame = buf.read()
+          d = dict(clients) # Make copy into local dict
+          for handle, sender in d.items():
+            try:
+              sender.send(frame)
+            except: # Client has gone away?
+              del clients[handle]
+  except KeyboardInterrupt: # ctrl-C
+    pass
+
+def flask_service(clients): # process 2
   from flask import Flask, Response
-  from time import sleep
   app = Flask(__name__)
 
   def frame_consumer():
-    while 1:
-      yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + frame.get() + b'\r\n'
-      sleep(1/60)
+    receiver, sender = Pipe(False)
+    clients[sender._handle] = sender
+    while True:
+      yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + receiver.recv() + b'\r\n'
 
   @app.route('/mjpeg')
   def mjpeg():
-    return Response(frame_consumer(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(frame_consumer(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-  # @app.route('/')
-  # def index():
-  #   return """
-  #   <body style="background: blue;">
-  #     <div style="width: 800px; margin: 0px auto;">
-  #       <img src="/mjpeg">
-  #     </div>
-  #   </body>
-  #   """
+  @app.route('/')
+  def index():
+    return """
+    <body style="background: blue;">
+      <div style="width: 800px; margin: 0px auto;">
+        <img src="/mjpeg">
+      </div>
+    </body>
+    """
 
-  app.run(host='192.168.43.150', threaded=True)
+  app.run(host='192.168.145.200', threaded=True)
 
 if __name__ == '__main__':
-  from multiprocessing import Process, Queue
-  from time import sleep
+  from multiprocessing import Process, Manager
 
-  frame = Queue()
+  with Manager() as manager:
+     clients = manager.dict()
 
-  p1 = Process(target=frame_collector, args=(frame,))
-  p2 = Process(target=flask_service, args=(frame,))
+     p1 = Process(target=frame_collector, args=(clients,))
+     p2 = Process(target=flask_service, args=(clients,))
+     p1.start()
+     p2.start()
 
-  p1.start()
-  p2.start()
-  print('All processes started!')
-  while 1:
-    try:
-      sleep(5)
-    except:
-      break
-  p1.terminate()
-  p2.terminate()
-  print('All processes terminated!')
+     try:
+       p2.join() # block while flask_service is running
+     except: # ctrl-C
+       p1.terminate()
+       p2.terminate()
+     finally:
+       print('Ending up.')
